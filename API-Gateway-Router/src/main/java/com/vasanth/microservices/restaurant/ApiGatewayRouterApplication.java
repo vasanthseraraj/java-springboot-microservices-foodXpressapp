b5ec32fd832e34943b24c670cb0de5303f030d74
Vasanth Seraraj <vasanthseraraj@gmail.com>
2023-06-23 19:44:27 +0530

    Implemented API-Gateway Routing mechanism

---
+++ API-Gateway-Router/src/main/java/com/vasanth/microservices/restaurant/ApiGatewayRouterApplication.java
@@ -0,0 +1,13 @@
+package com.vasanth.microservices.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayRouterApplication.class, args);
+	}
+
+}
---
+++ API-Gateway-Router/src/test/java/com/vasanth/microservices/restaurant/ApiGatewayRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasanth.microservices.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayRouterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
